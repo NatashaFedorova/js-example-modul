a9ee97ea6a5f1600a211c7c0f8b46b43de5ceb6f
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-06-16 01:06:33 +0300

    mod-1-webinar-1

--- js/dom.js
+++ js/dom.js
@@ -2,28 +2,28 @@
 // btnAdd.textContent = 'njnvzdjkfnbzkdfb';
 // console.log(btnAdd.textContent);
 
-const btnAdd = document.querySelector('button[data-add]');
-const valueInput = document.querySelector('input[data-value]');
-const outputEl = document.querySelector('span[data-output]');
-const btnReset = document.querySelector('button[data-reset]');
+// const btnAdd = document.querySelector('button[data-add]');
+// const valueInput = document.querySelector('input[data-value]');
+// const outputEl = document.querySelector('span[data-output]');
+// const btnReset = document.querySelector('button[data-reset]');
 
-console.log(outputEl);
+// console.log(outputEl);
 
-let total = 0;
+// let total = 0;
 
-btnAdd.addEventListener('click', function () {
-  const value = Number(valueInput.value);
-  console.log(value);
+// btnAdd.addEventListener('click', function () {
+//   const value = Number(valueInput.value);
+//   console.log(value);
 
-  total += value;
-  outputEl.textContent = total;
+//   total += value;
+//   outputEl.textContent = total;
 
-  valueInput.value = 0;
-});
+//   valueInput.value = 0;
+// });
 
-btnReset.addEventListener('click', function () {
-  total = 0;
-  outputEl.textContent = total;
-});
+// btnReset.addEventListener('click', function () {
+//   total = 0;
+//   outputEl.textContent = total;
+// });
 
 // получаем значение input во время click на кнопку
