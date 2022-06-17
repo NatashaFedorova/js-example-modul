051c3f73d8ae51510bc1268e1898bb923486f6d0
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-06-17 03:06:49 +0300

    autocheck (8)

---
+++ js/modul-1/autocheck-1.js
@@ -0,0 +1,66 @@
+// --------------------------Задача 1------------------------------------
+// Change code below this line
+// const productName = 'Droid';
+// console.log(productName);
+// 'Droid'
+// const pricePerItem = 2000;
+// console.log(pricePerItem);
+// 2000
+
+// --------------------------Задача 2------------------------------------
+// let productName = 'Droid';
+// productName = 'Repair droid';
+// let pricePerItem = 2000;
+// pricePerItem += 1500;
+
+// console.log(productName);
+// console.log(pricePerItem);
+
+// --------------------------Задача 3------------------------------------
+// const topSpeed = 160;
+// const distance = 617.54;
+// const login = 'mango935';
+// const isOnline = true;
+// const isAdmin = false;
+
+// console.log(topSpeed);
+// console.log(distance);
+// console.log(login);
+// console.log(isOnline);
+// console.log(isAdmin);
+
+// --------------------------Задача 4------------------------------------
+// const pricePerItem = 3500;
+// const orderedQuantity = 4;
+// const totalPrice = pricePerItem * orderedQuantity;
+// console.log(totalPrice);
+
+// --------------------------Задача 5------------------------------------
+// const productName = 'Droid';
+// const pricePerItem = 3500;
+// const message = `You picked ${productName}, price per item is ${pricePerItem} credits`;
+// console.log(message);
+
+// --------------------------Задача 6------------------------------------
+// const pricePerDroid = 800;
+// const orderedQuantity = 6;
+// const deliveryFee = 50;
+// const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+// const message = `You ordered droids worth ${totalPrice} credits. Delivery (${deliveryFee} credits) is included in total price.`;
+// console.log(message);
+
+// --------------------------Задача 7------------------------------------
+// function sayHi() {
+//   console.log('Hello, this is my first function!');
+// }
+// sayHi();
+
+// --------------------------Задача 8------------------------------------
+// function add(a, b, c) {
+//   console.log(`Addition result equals ${a + b + c}`);
+// }
+// add(15, 27, 10);
+// add(10, 20, 30);
+// add(5, 10, 15);
+
+// --------------------------Задача 9------------------------------------
