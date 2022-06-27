7a5e1edcaa8aeb8ac87aea3bb2bffa1f7c78d2c3
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-06-28 00:05:26 +0300

    mod-3-absract-3-add

--- js/modul-3/abstract-3-1.js
+++ js/modul-3/abstract-3-1.js
@@ -271,3 +271,186 @@
 // }
 
 //-------------------------------------Object.values()-----------------------------------
+// const book = {
+//   title: "The Last Kingdom",
+//   author: "Bernard Cornwell",
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+//-------------------------------------Object.values() - приклад 2-----------------------------------
+// const goods = {
+//   apples: 6,
+//   grapes: 3,
+//   bread: 4,
+//   cheese: 7,
+// };
+
+// const values = Object.values(goods); // [6, 3, 4, 7]
+
+// let total = 0;
+
+// for (const value of values) {
+//   total += value;
+// }
+
+// console.log(total); // 20
+
+//-------------------------------------Object.entries()-----------------------------------
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   rating: 8.38,
+// };
+// const keys = Object.keys(book);
+// console.log(keys); // ['title', 'author', 'rating']
+
+// const values = Object.values(book);
+// console.log(values); // ['The Last Kingdom', 'Bernard Cornwell', 8.38]
+
+// const entries = Object.entries(book);
+// console.log(entries);// [["title", "The Last Kingdom"], ["author", "Bernard Cornwell"], ["rating", 8.38]]
+
+//-------------------------------------Масив об'єктів-----------------------------------
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'На березі спокійних вод',
+//     author: 'Роберт Шеклі',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'Сон смішної людини',
+//     author: 'Федір Достоєвський',
+//     rating: 7.75,
+//   },
+// ];
+// =====================================
+// for (const book of books) {
+//   // Об'єкт книги (кожної книги)
+//   console.log(book); // {title: 'The Last Kingdom', author: 'Bernard Cornwell', rating: 8.38}
+//   // Назва (кожної книги)
+//   console.log(book.title); // The Last Kingdom
+//   // Автор (кожної книги)
+//   console.log(book.author); // Bernard Cornwell // Роберт Шеклі // Федір Достоєвський
+//   // Рейтинг (кожної книги)
+//   console.log(book.rating); // 8.38 // 8.51 // 7.75
+// }
+// ======================================
+// const bookNames = [];
+
+// for (const book of books) {
+//   bookNames.push(book.title);
+// }
+// console.log(bookNames);
+
+//--------------------------------spread: передача аргументів-----------------------------
+// const temps = [14, -4, 25, 8, 11];
+
+// // В консолі буде масив
+// console.log(temps);
+// // ❌ Так не спрацює, тому що передаємо цілий масив
+// console.log(Math.max(temps)); // NaN
+
+// // В консолі буде набір окремих чисел
+// console.log(...temps); // 14 -4 25 8 11
+// // ✅ Розподілимо колекцію елементів у якості окремих аргументів
+// console.log(Math.max(...temps)); // 25
+
+//--------------------------------spread: створення нового масиву---------------------------
+// const temps = [14, -4, 25, 8, 11];
+// // Це точна, але незалежна копія масиву temps
+// const copyOfTemps = [...temps];
+// console.log(copyOfTemps); // [14, -4, 25, 8, 11]
+
+// =================================================
+// const lastWeekTemps = [14, 25, 11];
+// const currentWeekTemps = [23, 17, 18];
+// const allTemps = [...lastWeekTemps, ...currentWeekTemps];
+// console.log(allTemps); // [14, 25, 11, 23, 17, 18]
+
+//--------------------------------spread: створення нового об'єкта---------------------------
+// const first = { propA: 5, propB: 10 };
+// const second = { propC: 15 };
+// const third = { ...first, ...second };
+// console.log(third); // {propA: 5, propB: 10, propC: 15}
+
+//-----------------------------spread: створення нового об'єкта (приклад 2)------------------
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15, propD: 20 };
+
+// const third = { ...first, ...second };
+// console.log(third); // { propA: 5, propB: 10, propC: 15, propD: 20 }
+
+// const fourth = { ...second, ...first };
+// console.log(fourth); // {propC: 50, propD: 20, propA: 5, propB: 10}
+
+//-----------------------------spread: створення нового об'єкта (приклад 3)------------------
+// const first = { propA: 5, propB: 10, propC: 50 };
+// const second = { propC: 15 };
+
+// const third = { propB: 20, ...first, ...second };
+// console.log(third); // {propB: 10, propA: 5, propC: 15}
+
+// const fourth = { ...first, ...second, propB: 20 };
+// console.log(fourth); // { propA: 5, propB: 20, propC: 15 }
+
+// const fifth = { ...first, propB: 30, ...second };
+// console.log(fifth); // {propA: 5, propB: 30, propC: 15}
+
+//-----------------------------rest: збирання всіх аргументів функції------------------------
+// Як оголосити параметри функції таким чином,
+// щоб можна було передати будь-яку кількість аргументів?
+// function multiply(...args) {
+//   console.log(args); // масив усіх аргументів
+// }
+
+// multiply(1, 2);
+// multiply(1, 2, 3);
+// multiply(1, 2, 3, 4);
+
+//--------------------------rest: збирання частини аргументів функції--------------------------
+// function multiply(firstNumber, secondNumber, ...otherArgs) {
+//   console.log(firstNumber); // Значення першого аргументу
+//   console.log(secondNumber); // Значення другого аргументу
+//   console.log(otherArgs); // Масив інших аргументів
+// }
+
+// multiply(1, 2);
+// // 1
+// // 2
+// // []
+// multiply(1, 2, 3);
+// // 1
+// // 2
+// // [3]
+// multiply(1, 2, 3, 4);
+// // 1
+// // 2
+// // [3, 4]
+
+//----------------------------------Деструктуризація об'єктів---------------------------------
+// const book = {
+//   title: 'The Last Kingdom',
+//   author: 'Bernard Cornwell',
+//   genres: ['historical prose', 'adventure'],
+//   isPublic: true,
+//   rating: 8.38,
+// };
+
+// // Деструктуризуємо
+// const { title, author, isPublic, rating, coverImage } = book;
+// console.log(coverImage); // undefined
+
+// const accessType = isPublic ? 'публічному' : 'закритому';
+// const message = `Книга ${title} автора ${author} з рейтингом ${rating} знаходиться в ${accessType} доступі.`;
+
+//----------------------------------Значення за замовчуванням---------------------------------
