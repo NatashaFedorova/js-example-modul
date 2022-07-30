e90b3cfbb0855acd7cf478b8107ab7477cab99b7
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-07-30 22:38:59 +0300

    mod-6-repeta-1

---
+++ data/product.js
@@ -0,0 +1,43 @@
+export default [
+  {
+    name: 'Lorem',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+    price: 1000,
+    available: true,
+    onSale: true,
+  },
+  {
+    name: 'Apple',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+    price: 2000,
+    available: true,
+    onSale: false,
+  },
+  {
+    name: 'Pear',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+    price: 3000,
+    available: true,
+    onSale: true,
+  },
+  {
+    name: 'Cherry',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+    price: 4000,
+    available: true,
+    onSale: false,
+  },
+
+  {
+    name: 'Mango',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+    price: 5000,
+    available: false,
+    onSale: false,
+  },
+];
--- js/modul-6/modul-6-repeta-1.js
+++ js/modul-6/modul-6-repeta-1.js
@@ -1,3 +1,7 @@
+import products from '../../data/product.js';
+import transaction from '../../data/transaction.js';
+import transactionHistory from '../../data/transaction.js';
+
 // ========================для прикладу 1 (repeta-1) пошук елемента==============================
 // const navLiEl = document.querySelector('li');
 // console.log(navLiEl); // перший li
@@ -162,4 +166,206 @@
 // const heroEl = document.querySelector('.hero');
 // heroEl.append(titleEl, imgEl);
 
-// ========================для прикладу 8 (repeta-1) створення колекції==============================
+// ========================для прикладу 8 (repeta-1) створення колекції елементів==============================
+// для кожного об'екта масива має бути сворена кнопка
+
+// const colorPickerOptions = [
+//   { label: 'red', color: '#ff0000' },
+//   { label: 'green', color: '#008000' },
+//   { label: 'blue', color: '#0000ff' },
+//   { label: 'grey', color: '#808080' },
+//   { label: 'pink', color: '#ffc0cb' },
+//   { label: 'indigo', color: '#4b0082' },
+// ];
+
+// const colorPickerContainerEl = document.querySelector('.js-color-picker');
+
+// застарілий спосіб====================================
+// const elements = [];
+// for (let i = 0; i < colorPickerOptions.length; i += 1) {
+//   const option = colorPickerOptions[i];
+
+//   const buttonEl = document.createElement('button');
+//   buttonEl.type = 'button';
+//   buttonEl.classList.add('color-picker__option');
+//   buttonEl.textContent = option.label;
+//   buttonEl.style.backgroundColor = option.color;
+//   elements.push(buttonEl);
+// }
+// console.log(elements);
+
+// colorPickerContainerEl.append(...elements);
+
+// застарілий спосіб====================================
+// const elements = colorPickerOptions.map(option => {
+//   const buttonEl = document.createElement('button');
+//   buttonEl.type = 'button';
+//   buttonEl.classList.add('color-picker__option');
+//   buttonEl.textContent = option.label;
+//   buttonEl.style.backgroundColor = option.color;
+//   return buttonEl;
+// });
+
+// console.log(elements);
+
+// colorPickerContainerEl.append(...elements);
+
+// функція для створення розмітки colorPicker============
+
+// const makeColorPickerContainerEl = options => {
+//   return options.map(option => {
+//     const buttonEl = document.createElement('button');
+//     buttonEl.type = 'button';
+//     buttonEl.classList.add('color-picker__option');
+//     buttonEl.textContent = option.label;
+//     buttonEl.style.backgroundColor = option.color;
+//     return buttonEl;
+//   });
+// };
+
+// const elements = makeColorPickerContainerEl(colorPickerOptions);
+// colorPickerContainerEl.append(...elements);
+
+// ========================для прикладу 9 (repeta-1) створення картки продукта==============================
+// в картці має бути product__on-sale, product__not-available
+
+// const product = {
+//   name: 'Lorem',
+//   description:
+//     'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta nostrum earum blanditiis, ratione quod accusantium eligendi neque non dolores perferendis officiis? Deserunt officiis minus eligendi laborum consectetur, quia esse doloremque!',
+//   price: 2000,
+//   available: true,
+//   onSale: true,
+// };
+
+/*
+<article class="product">
+* <h2 class="product__name">Назва</h2>
+* <p class="product__descr">Опис</p>
+* <p class="product__price">Ціна:</p>
+</article>
+*/
+
+// const productEl = document.createElement('article');
+// productEl.classList.add('product');
+
+// const productNameEl = document.createElement('h2');
+// productNameEl.classList.add('product__name');
+// productNameEl.textContent = product.name;
+
+// const productDescriptionEl = document.createElement('p');
+// productDescriptionEl.classList.add('product__descr');
+// productDescriptionEl.textContent = product.description;
+
+// const productPriceEl = document.createElement('p');
+// productPriceEl.classList.add('product__price');
+// productPriceEl.textContent = `Ціна: ${product.price} кредитов`;
+
+// productEl.append(productNameEl, productDescriptionEl, productPriceEl);
+
+// console.log(productEl);
+
+// ========================для прикладу 10 (repeta-1) створення картки продукта==============================
+//розкоментувари 1 строку в цьому файлі (імпорт массиву об'єктів products)
+// console.log(products);
+
+// const makeProductCard = ({ name, description, price }) => {
+//   const productEl = document.createElement('article');
+//   productEl.classList.add('product');
+
+//   const productNameEl = document.createElement('h2');
+//   productNameEl.classList.add('product__name');
+//   productNameEl.textContent = name;
+
+//   const productDescriptionEl = document.createElement('p');
+//   productDescriptionEl.classList.add('product__descr');
+//   productDescriptionEl.textContent = description;
+
+//   const productPriceEl = document.createElement('p');
+//   productPriceEl.classList.add('product__price');
+//   productPriceEl.textContent = `Ціна: ${price} кредитов`;
+
+//   productEl.append(productNameEl, productDescriptionEl, productPriceEl);
+
+//   return productEl;
+// };
+
+// const productCards = products.map(makeProductCard); // makeProductCard - це колбек-функція для кожного елемента, який перебирається
+// console.log(productCards);
+
+// const productsContainer = document.querySelector('.js-products');
+// productsContainer.append(...productCards);
+
+// ========================для прикладу 11 (repeta-1) парс строк=============================
+// const titleEl = document.querySelector('.title');
+// console.log(titleEl); // <h1 class="title">Заголовок <span>;)</span></h1>
+// console.log(titleEl.textContent); // Заголовок ;)
+// console.log(titleEl.innerHTML); // Заголовок <span>;)</span>
+
+// презаписуємо варіант 1
+// titleEl.innerHTML = 'qwertyuiio';
+// console.log(titleEl); // <h1 class="title">qwertyuiio</h1>
+
+// презаписуємо варіант 2
+// titleEl.innerHTML = '<a href="#">Заголовок-посилання</a>';
+// console.log(titleEl); // <h1 class="title"><a href="#">Заголовок-посилання</a></h1>
+
+// очищуємо тег - кращий вариант очищенння
+// titleEl.innerHTML = ''; // <h1 class="title"></h1>
+// console.log(titleEl);
+
+// додаємо  - ТАК НЕ РОБИТИ
+
+// titleEl.innerHTML += 'Додана частина'; // <h1 class="title"></h1>
+// console.log(titleEl);
+
+// insertAdjacentHTML==========================================
+// titleEl.insertAdjacentHTML('afterbegin', 'Доданий тект! ');
+// console.log(titleEl); // <h1 class="title">Доданий тект! Заголовок <span>;)</span></h1>
+
+// titleEl.insertAdjacentHTML('beforeend', ' Доданий тект!');
+// console.log(titleEl); // <h1 class="title">Заголовок <span>;)</span> Доданий тект!</h1>
+
+// const smileEl = document.querySelector('.title__smile');
+// smileEl.insertAdjacentHTML('afterbegin', '<a href="#" class="title__link">посилання</a>');
+// console.log(smileEl); // <h1 class="title">Доданий тект! Заголовок <span>;)</span></h1>
+
+// ========================для прикладу 11 (repeta-1) таблиця транзакцій строк=============================
+//розкоментувари 2 строку в цьому файлі (імпорт массиву об'єктів transactionsHistory)
+console.log(transactionHistory);
+
+// makeTransactionTableRowMarkup(переклад) - зробити розмітку рядка таблиці транзакцій
+/*
+ * <tbody>
+ *   <tr>
+ *     <td>ID транзакции</td>
+ *     <td>Сумма</td>
+ *     <td>Дата</td>
+ *     <td>Кто</td>
+ *     <td>Тип транзации</td>
+ *     <td>Имя счёта</td>
+ *     <td>Номер счёта</td>
+ *   </tr>
+ * </tbody>
+ */
+
+const makeTransactionTableRowMarkup = transaction => {
+  const { id, amount, date, business, type, name, account } = transaction;
+  return `
+  <tr>
+    <td>${id}</td>
+    <td>${amount}</td>
+    <td>${date}</td>
+    <td>${business}</td>
+    <td>${type}</td>
+    <td>${name}</td>
+    <td>${account}</td>
+   </tr>
+ `;
+};
+
+const makeTransactionTableRows = transactionHistory.map(makeTransactionTableRowMarkup).join('');
+console.log(makeTransactionTableRows);
+
+const tableBodyEl = document.querySelector('.table__body');
+tableBodyEl.insertAdjacentHTML('afterbegin', makeTransactionTableRows);
