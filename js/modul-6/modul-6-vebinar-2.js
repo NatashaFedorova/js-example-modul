cb97b276db2a7afc14e82d6b98112b5c00bf84f9
NatashaFedorova <dyadyafedor1287@gmail.com>
2022-08-02 02:27:04 +0300

    mod-6-vebinar-end

---
+++ js/modul-6/modul-6-vebinar-2.js
@@ -0,0 +1,12 @@
+// =====================приклад 1==============================
+const refs = {
+  fakeLink: document.querySelector('.fake-link'),
+};
+
+const handleFakeLinkClick = e => {
+  e.preventDefault();
+  window.location = 'https://github.com/NatashaFedorova/todo-list';
+  console.log('e...');
+};
+
+refs.fakeLink.addEventListener('click', handleFakeLinkClick);
